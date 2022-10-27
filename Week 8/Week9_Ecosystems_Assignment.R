79453ac62f0af87a99ec1483e9ef23aca71e1b1d
mlrolecki <rolecki@susqu.edu>
2022-10-27 09:54:23 -0400

    ah

--- Week 8/Week9_Ecosystems_Assignment.R
+++ Week 8/Week9_Ecosystems_Assignment.R
@@ -6,7 +6,7 @@
 
 library(readxl)
 
-setwd("C:/GitHub/R4Eco_2022/Week9")
+setwd("C:/GitHub/Rolecki-R4Ecol/Week 8")
 
 abiotic.tibble <- read_excel("Penaetal_2016_data.xlsx", sheet = "Abiotic factors")
 abiotic <- as.data.frame(abiotic.tibble)
@@ -28,14 +28,39 @@ invert.means <- aggregate(x = invert, by = list(invert$names), FUN = "mean")
 head(abiotic.means)
 head(invert.means)
 
-abiotic.means1 <- abiotic.means[,-16] 
-abiotic.means2 <- abiotic.means1[,-1:-6] 
-abiotic.means2 <- sapply(abiotic.means2, as.numeric ) 
-abiotic.means2 <- as.data.frame(abiotic.means2) 
+abiotic.invert <- merge(abiotic.means, invert.means, by = "Group.1", all = FALSE)
 
-invert.means1 <- invert.means[,-1:-3]
-invert.means2 <- invert.means1[,-
+abiotic.unmerged <- abiotic.invert[,7:15]
+invert.unmerged <- abiotic.invert[,19:50]
 
+
+#abiotic.means1 <- abiotic.unmerged[,-16] 
+#abiotic.means2 <- abiotic.means1[,-1:-6] 
+#abiotic.means1 <- sapply(abiotic.means2, as.numeric ) 
+#abiotic.means1 <- as.data.frame(abiotic.means2) 
+
+#invert.means1 <- invert.unmerged[,-1:-3]
+#invert.means2 <- invert.means1[,-70]
+#invert.means2 <- sapply(invert.means2, as.numeric)
+#invert.means2 <- as.data.frame(invert.means2)
+
+
+abiotic.unmerged1<- sapply(abiotic.unmerged, as.numeric)
+abiotic.unmerged1 <- as.data.frame(abiotic.unmerged1)
+invert.unmerged1 <- sapply(invert.unmerged, as.numeric)
+invert.unmerged1 <- as.data.frame(invert.unmerged1)
+
+
+abiotic.means$Group.1==invert.means$Group.1
+
+
+library(vegan)
+colnames(abiotic.unmerged)
+ord <- rda(invert.unmerged1 ~ pH + totalN + Perc_ash + Kalium + Magnesium + Ca + Al + TotalP + OlsenP, abiotic.unmerged1)
+ord
+
+anova(ord)
+plot(ord)  
 # (Q2 - 12 pts) Then use the dataset from the tutorial to create a linear model related to your RDA. Try multiple predictors to find the best fit model.
   # Explain the ecological importance of the significant predictors, or lack of significant predictors.
 
