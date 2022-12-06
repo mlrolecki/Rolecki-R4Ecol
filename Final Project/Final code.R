1661b5e90095efffa8f197869dcd03edf87f74b3
mlrolecki <rolecki@susqu.edu>
2022-12-06 08:36:20 -0500

    Starting final code

---
+++ Final Project/Final code.R
@@ -0,0 +1,3 @@
+#Final project code!
+#I have concerted all of my data into excel form
+#Next I must read it in, merge the data, and figure out how to get locations to match up between the weather and bird data
