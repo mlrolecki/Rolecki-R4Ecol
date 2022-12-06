5e40e7895676829d4500b572722c2f678c50a6dd
mlrolecki <rolecki@susqu.edu>
2022-12-06 09:38:52 -0500

    trying to load in data

--- Final Project/Final code.R
+++ Final Project/Final code.R
@@ -1,3 +1,16 @@
 #Final project code!
 #I have concerted all of my data into excel form
-#Next I must read it in, merge the data, and figure out how to get locations to match up between the weather and bird data
+#Next I must read it in (done!), merge the data, and figure out how to get locations to match up between the weather and bird data
+
+library(readxl)
+setwd("C:/GitHub/Rolecki-R4Ecol/Final Project")
+
+curlew.data <- read_excel("Curlew data.xlsx", sheet = "Edited Curlew")
+weather.data <- read_excel("Curlew data.xlsx", sheet = "Weather")
+
+#weather data and curlew data matched up in four locations 
+
+curlew <- as.data.frame(curlew.data)
+weather <- as.data.frame(weather.data)
+
+
