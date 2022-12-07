20e97e6b4ac3bef5569e9c2072f6b9baee96645f
mlrolecki <rolecki@susqu.edu>
2022-12-07 16:42:46 -0500

    trying to merge

--- Final Project/Final code.R
+++ Final Project/Final code.R
@@ -13,4 +13,25 @@ weather.data <- read_excel("Curlew data.xlsx", sheet = "Weather")
 curlew <- as.data.frame(curlew.data)
 weather <- as.data.frame(weather.data)
 
+#RENAMING COLUMN NAMES TO MATCH
 
+'COUNTY' <- names(weather)[names(weather) == 'site'] 
+names(weather)
+
+#Separating dates into yr month day; renaming to match in each dataset
+#Cannot figure out why COUNTY yyyy and mm show up empty?????
+?strsplit
+
+strsplit(list(curlew$`OBSERVATION DATE`), "-")
+
+dates <- data.frame(do.call("rbind", strsplit(as.character(curlew$`OBSERVATION DATE`), "-", fixed = TRUE)))
+"yyyy" <- names(curlew)[names(curlew) == 'X1'] 
+"mm" <- names(curlew)[names(curlew) == 'X2']
+
+Location.date <- cbind
+
+
+curlew.weather <- merge(curlew.data, weather.data, by cbind(COUNTY, yyyy, mm))
+
+
+  
