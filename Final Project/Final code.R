cded5b7124189d6d812724d631edf1b645b3e00f
mlrolecki <rolecki@susqu.edu>
2022-12-08 09:45:29 -0500

    Update Final code.R

--- Final Project/Final code.R
+++ Final Project/Final code.R
@@ -1,6 +1,6 @@
 #Final project code!
 #I have concerted all of my data into excel form
-#Next I must read it in (done!), merge the data, and figure out how to get locations to match up between the weather and bird data
+#Next I must read it in, merge the data, and figure out how to get locations to match up between the weather and bird data
 
 library(readxl)
 setwd("C:/GitHub/Rolecki-R4Ecol/Final Project")
@@ -19,27 +19,39 @@ names(weather)[names(weather) == 'site'] <-"COUNTY"
 names(weather)
 
 #Separating dates into yr month day; renaming to match in each dataset
-#Cannot figure out why COUNTY yyyy and mm show up empty?????
-#Matt thank you so much if you had not helped me fix my merge data I would have had a breakdown I appreciate you :)
+#Cannot figure out why COUNTY yyyy and mm show up empty????? <- code error!
+#Matt thank you so much if you had not helped me fix my merge data I would have failed :)
 
 ?strsplit
+#This will be used to split date into separate year/month/day columns
 
 strsplit(list(curlew$`OBSERVATION DATE`), "-")
 
 dates <- data.frame(do.call("rbind", strsplit(as.character(curlew$`OBSERVATION DATE`), "-", fixed = TRUE)))
 
-
+#Renaming split columns
 names(dates)[names(dates) == 'X1'] <- 'yyyy' 
 names(dates)[names(dates) == 'X2'] <- "mm"
 names(curlew)[names(curlew) == "COUNTY"] <- "Site"
 
+#Merging dates
 curlew.date <- cbind(curlew,dates)
 
-
+#Merging dates and locations
 curlew.weather <- merge(curlew.date, weather, by = c("Site", "yyyy","mm"))
 
 
 #NOW, DATA ANALYSIS!!!
+#Getting rid of non-numeric in curlew count data:
+curlew.data$`OBSERVATION COUNT` <- gsub("X", "NA", curlew.data$`OBSERVATION COUNT`)
+curlew.data.obscount1 <- subset(curlew.data, curlew.data$`OBSERVATION COUNT` !="NA")
+as.data.frame(curlew.data.obscount1)
+num.curlew.data <-as.numeric(as.character(curlew.data.obscount1$`OBSERVATION COUNT`))
 
+
+#Histogram- shows the frequency of occurrance in terms of how many individuals occur together
+#Shows that the birds are often found in low numbers (individual, pair, small group) but that there are likely breeding events that take place infrequently with hundreds of individuals. 
+hist(num.curlew.data, xlab = "Curlew Count", ylab= "Frequency", col = 'skyblue3', breaks= 50, main= "Curlew Count Frequency")
+?hist
 
   
